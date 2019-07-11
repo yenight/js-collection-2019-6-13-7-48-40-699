70ed0abbfd17262d4bac474e9f1c05eff379aa96
williamye <william.ye@oocl.com>
2019-07-11 20:12:41 +0800

    feat: filter grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function grouping_count(collection) {
-
+  const countNumber = {'1':0, '2':0, '3':0, '4':0}
+  collection.filter(value => {
+    countNumber[value + '']++
+  })
+  return countNumber
 }
 
 module.exports = grouping_count;
